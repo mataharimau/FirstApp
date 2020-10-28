41f9a6a360d9eb79b2aa31ff3c18197e3456cb3c
mataharimau <audrey.tam@gmail.com>
2020-10-29 08:20:25 +1100

    Initial Commit

---
+++ FirstApp/ByeView.swift
@@ -0,0 +1,20 @@
+//
+//  ByeView.swift
+//  FirstApp
+//
+//  Created by Audrey Tam on 28/10/20.
+//
+
+import SwiftUI
+
+struct ByeView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct ByeView_Previews: PreviewProvider {
+    static var previews: some View {
+        ByeView()
+    }
+}
---
+++ FirstApp/FirstAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  FirstAppApp.swift
+//  FirstApp
+//
+//  Created by Audrey Tam on 28/10/20.
+//
+
+import SwiftUI
+
+@main
+struct FirstAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
